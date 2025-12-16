6beaccfe278091224c0d368d77d1e09a8013b59d
Trinh Ngoc Dai <daingoctrinh72711@gmail.com>
2025-12-16 09:15:14 +0700

    Connect to Mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = 'mongodb://localhost:27017/bongphim_db'; // Thay thế bằng URI của bạn
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/bongphim';
 
 // Middleware cơ bản
 app.use(express.json());
