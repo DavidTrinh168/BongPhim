a05e0440f8af0d8604eb1ffaaabda4512c088c8a
Trịnh Ngọc Đại <daingoctrinh72711@gmail.com>
2026-08-27 16:41:45 +0700

    Feat: Add Mongoose models index

---
+++ server/src/models/Index.ts
@@ -0,0 +1,10 @@
+import './Country.js';
+import './Crew.js';
+import './Episode.js';
+import './Genre.js';
+import './Movie.js';
+import './User.js';
+import './watchHistory.js';
+import './watchList.js';
+
+console.log('📦 Tất cả Mongoose Models đã được nạp thành công!');
